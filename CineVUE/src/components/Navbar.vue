f85c6acad0995c859e4dab3338b5d3ac37f8c492
Fabio <fp3196@gmail.com>
2025-03-06 17:04:41 +0100

    Lezione 2 senza esercizi

---
+++ CineVUE/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav>
+      <ul>
+          <li><RouterLink to="/">Home</RouterLink></li>
+          <li><RouterLink to="/about">About</RouterLink></li>
+          <li><a href="contatti.html">Contatti</a></li>
+          <li><a href="news.html">News</a></li>
+          <li><a href="who.html">Chi siamo</a></li>
+      </ul>
+  </nav>
+</template>
---
+++ CineVUE/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Navbar from '../components/Navbar.vue'
+import Header from '@/components/Header.vue';
+</script>
+
+<template>
+    <Header></Header>
+    <Navbar></Navbar>
+  
+  <h2>Benvenuti in questo sito!</h2>
+  <p>Su <a href="about.html"><i>Cineva</i></a> potete scrivere recensioni su <u>film e serie tv</u>
+      e condividere i vostri pensieri con la <b>community</b>!</p>
+
+  <hr>
+  <h2>Recensioni in evidenza</h2>
+  <h3>MommyDemi - "Anora"? No grazie</h3>
+
+  <p>Ma davvero? Davvero Mikey Madison ha portato a casa l’Oscar per la miglior attrice protagonista mentre The Substance e una straordinaria Demi Moore sono state ignorate? Siamo davanti all'ennesima dimostrazione che l’Academy è un circo autoreferenziale, più interessato a seguire mode del momento che a premiare il vero talento.</p>
+  <p><b>Giudizio:</b> 1/5 - <b>Likes:</b> 1.2k - <b>Dislikes:</b> 392</p>
+</template>
+
+
+
